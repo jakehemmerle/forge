9f46b3603f587f1d5c6d1de1cf455b4a7e5eaee3
Jake Hemmerle <jakehemmerle@gmail.com>
2014-10-29 16:52:08 -0400

    Renamed com.assets.letsmod to com.assets.jakehemmerle

--- src/main/java/com/jakehemmerle/letsmod/reference/Reference.java
+++ src/main/java/com/jakehemmerle/letsmod/reference/Reference.java
@@ -1,10 +1,10 @@
 package com.jakehemmerle.letsmod.reference;
 
 public class Reference {
-	public static final String MOD_ID = "letsmod";
+	public static final String MOD_ID = "jakehemmerle";
 	public static final String MOD_NAME = "Jake Hemmerle's Motherfucking Mod";
-	public static final String SERVER_PROXY_CLASS = "com.jakehemmerle.letsmod.proxy.ServerProxy";
-	public static final String CLIENT_PROXY_CLASS = "com.jakehemmerle.letsmod.proxy.ClientProxy";
-	public static final String GUI_FACTORY_CLASS = "com.jakehemmerle.letsmod.client.gui.GuiFactory";
+	public static final String SERVER_PROXY_CLASS = "com.jakehemmerle.jakehemmerle.proxy.ServerProxy";
+	public static final String CLIENT_PROXY_CLASS = "com.jakehemmerle.jakehemmerle.proxy.ClientProxy";
+	public static final String GUI_FACTORY_CLASS = "com.jakehemmerle.jakehemmerle.client.gui.GuiFactory";
 	public static final String VERSION = "1.7.10-1.0";
 }
