10c1c9bac5bd5adf6289b5f0833ae516891d523c
Jake Hemmerle <jakehemmerle@gmail.com>
2014-11-05 20:07:06 -0500

    Corrected names in 'en_US' localization and 'Reference' class.

--- src/main/java/com/jakehemmerle/letsmod/reference/Reference.java
+++ src/main/java/com/jakehemmerle/letsmod/reference/Reference.java
@@ -2,7 +2,7 @@ package com.jakehemmerle.letsmod.reference;
 
 public class Reference {
 	public static final String MOD_ID = "letsmod";
-	public static final String MOD_NAME = "Jake Hemmerle's Motherfucking Mod";
+	public static final String MOD_NAME = "Let's Mod!";
 	public static final String SERVER_PROXY_CLASS = "com.jakehemmerle.letsmod.proxy.ServerProxy";
 	public static final String CLIENT_PROXY_CLASS = "com.jakehemmerle.letsmod.proxy.ClientProxy";
 	public static final String GUI_FACTORY_CLASS = "com.jakehemmerle.letsmod.client.gui.GuiFactory";
